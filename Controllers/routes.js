25e66943294864d23765cec9f23bfae895c8f592
ravenf12 <ravenfisher1122@gmail.com>
2023-06-15 14:16:30 -0600

    Put in patch/delete and added comments

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -19,10 +19,15 @@ router.get("/", (req, res) => {
   }
 });
 
+// Get by ID
+// http://localhost:5001/routes/:id
 router.get("/:id", (req, res) => {
   try {
+    // Getting by ID
     const id = req.params.id;
+    // 
     let routes = blog.filter((obj) => obj.id == id);
+    // A message for our mistakes
     res.status(200).json({
       status: `Author at id: ${id}`, routes,
     });
@@ -33,34 +38,15 @@ router.get("/:id", (req, res) => {
   }
 });
 
-router.get("/title/:title", (req, res) => {
-  try {
-    const newTitle = req.params.class;
 
-    let titleResults = blog.filter ((obj) => obj.titleType.toUpperCase() === newTitle.toUpperCase());
-
-    if (titleResults === 0) {
-      res.status(401).json({
-        message: `No title ${newTitle}`,
-      });
-    }
-    res.status(200).json({
-      title: titleResults,
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-    });
-  }
-});
-
-// http://localhost:5001/title/create
+// http://localhost:5001/routes/create
 router.post("/create", (req, res) => {
   try {
-    let { newTitle, author, body} = req.body;
+    // Our body for what we want to write for the creation
+    let { newTitle, author, body } = req.body;
 
     let newId = blog.length + 1;
-
+    // 
     const oneTitle = {
       id: newId,
       newTitle,
@@ -71,6 +57,7 @@ router.post("/create", (req, res) => {
     fs.readFile(fsPath, (err, data) => {
       const database = JSON.parse(data);
 
+      // IDS for out database? I honestly don't remember...
       let currentIDs = [];
       database.forEach((obj) => {
         currentIDs.push(obj.id);
@@ -85,6 +72,7 @@ router.post("/create", (req, res) => {
 
       fs.writeFile(fsPath, JSON.stringify(database), (err) => console.log(err));
 
+      // Gives us a message
       res.status(200).json({
         status: `New Title ${oneTitle.name}.`,
         oneTitle,
@@ -97,6 +85,68 @@ router.post("/create", (req, res) => {
   }
 });
 
-// Giving in for now, finally debugged whatever I had done to Nodemon to make it run in a continuos loop. Now my issue is postman.. Im getting "404 - not found" errors. So it's still pulling, just not the information. This is about where Im at with servers.. Im going to dive back in tomorrow to see if I can figure this one out.
+// http://localhost:5001/routes/edit
+router.patch("/edit", async (req, res) => {
+  try {
+    let Route;
+      // Pulling the values from the url & req.body 
+      const { id } = req.params;
+      const { body } = req.body; // only want the user to be able to change what they said, not who said it, or where they said it
+
+      // Make a filter
+      const filter = {_id: id};
+
+      // Check for only what the user wants to change about the message (new stuff)
+      const returnOption = {new: true};
+
+      // Passing in new info!!
+      const updateRoute = await Route.findOneAndUpdate(
+          filter,
+          { body }, // needs to be an object to work properly
+          returnOption
+      );
+
+      // Check to see if we were able to update
+      updateRoute ?
+          res.status(200).json({
+              // if the message was found and updated
+              message: `Updated!`, updateRoute,
+          })
+          : res.status(404).json({
+              // if the message was not found and/or did not belong to the user
+              message: `Can't find..`
+          });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+// http://localhost:5001/routes/title
+router.delete('/title', async (req,res) => {
+  try {
+      // Pulling some value
+      const { id } = req.params;
+      // Delete the route 
+      const deleteRoutes = await routes.deleteOne({_id: id, owner_id: req.user._id});
+
+      // Send the response based on whether we deleted something
+      deleteRoutes.deletedCount === 1 ?
+          res.status(200).json({
+              // if deleted
+              message: "Deleted!"
+          })
+          : res.status(404).json({
+              // if not deleted
+              message: "I can't find that.."
+          });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+}); 
+
 
   module.exports = router;
--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+// Import stuff
 const express = require("express");
 require("dotenv").config();
-
 const app = express();
-
 const PORT = 5001;
-
 const fs = require("fs");
+// Mongoose
+const mongoose = require("mongoose");
+mongoose.set(`strictQuery`, false);
+const MONGO = process.env.MONGODB;
+mongoose.connect(`${MONGO}`);
 
+// Controller variable 
 const routes = require("./Controllers/routes.js");
 
+// App.use
 app.use(express.json());
-
 app.use("/routes", routes);
-// http://localhost:5000/blog
+// http://localhost:5001/routes
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
+
+
+  // Note to self, don't add extra into "app.js", it will throw off nodemon. 
